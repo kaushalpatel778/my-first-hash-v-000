500e32e722458c69550bd6a202f81f3676bd6c14
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-21 19:12:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice. 
+  your_hash = {"Apple" => "green", "Grape" => "red","Mango" => "yellow"}
+  
+              
   
 end
 
